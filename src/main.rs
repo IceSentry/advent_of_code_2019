c09363e57962d987bc2d5a4514ede39960cc12eb
IceSentry <c.giguere42@gmail.com>
2019-12-05 23:17:34 -0500

    day 2 completed

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 
 fn main() {
-    day_2();
+    day_2_part_2();
 }
 
 fn day_1() {
@@ -26,16 +26,94 @@ fn day_1() {
         total += fuel;
     }
 
-    output
-        .write_all(total.to_string().as_bytes())
-        .expect("Unable to write result");
+    write!(output, "{}", total).expect("Unable to write result");
+}
+
+fn parse_opcode(input: &mut Vec<usize>) {
+    for index in (0..input.len()).step_by(4) {
+        match input[index] {
+            1 => {
+                let value1 = input[index + 1];
+                let value2 = input[index + 2];
+                let value3 = input[index + 3];
+                input[value3] = input[value1] + input[value2];
+            }
+            2 => {
+                let value1 = input[index + 1];
+                let value2 = input[index + 2];
+                let value3 = input[index + 3];
+                input[value3] = input[value1] * input[value2];
+            }
+            99 => break, // terminate
+            _ => continue,
+        }
+    }
 }
 
 fn day_2() {
     let input = fs::read_to_string("src/2/input.txt").expect("Unable to open input.txt");
     let mut output = File::create("src/2/output.txt").expect("Unable to create output.txt");
 
-    output
-        .write_all(&input.as_bytes())
-        .expect("Unable to write result");
+    let mut input: Vec<usize> = input
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    input[1] = 12;
+    input[2] = 2;
+
+    parse_opcode(&mut input);
+
+    // println!(
+    //     "{}",
+    //     input
+    //         .iter()
+    //         .map(|x| x.to_string())
+    //         .collect::<Vec<String>>()
+    //         .join(",")
+    // );
+
+    write!(output, "{}", input[0]).expect("Unable to write result");
+}
+
+fn find_pair(input: &Vec<usize>, target_value: usize) -> (usize, usize) {
+    let mut noun = 0;
+    let mut verb = 0;
+
+    loop {
+        loop {
+            let mut input_copy = input.clone();
+
+            input_copy[1] = noun;
+            input_copy[2] = verb;
+
+            parse_opcode(&mut input_copy);
+
+            if input_copy[0] == target_value {
+                return (noun, verb);
+            } else if verb >= 99 {
+                break;
+            }
+
+            verb += 1;
+        }
+        verb = 0;
+        noun += 1;
+    }
+}
+
+fn day_2_part_2() {
+    let input = fs::read_to_string("src/2/input.txt").expect("Unable to open input.txt");
+    let mut output = File::create("src/2/output.txt").expect("Unable to create output.txt");
+
+    let input: Vec<usize> = input
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    let (noun, verb) = find_pair(&input, 19690720);
+
+    let result = 100 * noun + verb;
+
+    write!(output, "{}", result).expect("Unable to write result");
 }
